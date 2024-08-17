30bf2f6a6fd6ceb86c3bd8ec7fa82517d1cce58e
watercubz <eurysosagarcia@gmail.com>
2024-08-17 00:00:18 -0400

    feat(CORS): cors implementation

--- services/UserService/src/server.ts
+++ services/UserService/src/server.ts
@@ -1,13 +1,18 @@
-import Express, {  Request, Response } from "express";
-
+import Express, {  Request, Response, json } from "express";
+import users from "../src/router/user.routes"
+import { valideMiddleware } from "./middleware/cors";
 
 const app = Express()
+app.use(Express.json())
+valideMiddleware()
 
-const PORT = process.env.PORT ?? 5000
+app.use("/api/v1", users)
 
-app.use("/", (req: Request, res: Response) => {
- res.send("Hello")
-})
+// app.use("/", (req: Request, res: Response) => {
+ //  res.send("Hewllo")
+ // })
+
+ const PORT = process.env.PORT ?? 5000
 
 app.listen(PORT, () => {
  console.log(`server is running on port http://localhost:${PORT}`);
