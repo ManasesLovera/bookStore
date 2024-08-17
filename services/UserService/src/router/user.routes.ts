3072289df0cecab3c03ca5dda5605056127cf30b
watercubz <eurysosagarcia@gmail.com>
2024-08-17 00:57:16 -0400

    feat(dotenv): set environment variables

--- services/UserService/src/router/user.routes.ts
+++ services/UserService/src/router/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createUser, getAllUser } from "../controller/user.controller";
+import { createUser, getAllUser, Login } from "../controller/user.controller";
 
 const router = Router()
 
+router.post("/login", Login)
 router.post("/users", createUser)
 router.get("/users", getAllUser)
 
