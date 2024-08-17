9ff8398554093eff4f956dd8078cd311ec89f9a3
watercubz <eurysosagarcia@gmail.com>
2024-08-17 00:51:22 -0400

    feat(JWT): authentication implementation with jwt

--- services/UserService/src/controller/user.controller.ts
+++ services/UserService/src/controller/user.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express"
 import { pool } from "../database/postgres"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const JWT_SECRET = process.env.SECRET_KEY || "7238ad21dcd7802a527bdb1701b12f89"
 
 export const getAllUser = async (req: Request, res: Response) => {
  try {
@@ -31,3 +35,27 @@ export const createUser = async (req: Request, res: Response) => {
    }
 }
 
+export const Login = async (req: Request, res: Response) => {
+ const { email, password} = req.body 
+  try {
+    const result = await pool.query("SELECT * FROM  users WHERE email = $1", [email])
+     
+    const user =  result.rows[0];
+
+    if(result.rows.length === 0) {
+     return res.status(401).json({message: "Invalid credential"})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+      return res.status(401).json({message: "Invalid credencials"})
+    }
+   
+    const token  = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: "24hr"})
+
+    res.json({token})
+  } catch (error) {
+   return res.status(500).json({message: "Internal Erorr Database"})
+  }
+}
