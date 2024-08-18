8d4d2b6ba2f8e571624535df51d232390813a4f0
ricardocollado <ricardocolladorothschild@gmai.com>
2024-08-18 16:46:20 -0400

    Several works: a) dockernization of order-services b) add some c# code to the order-service,s basically to have an env variable available and add port that shoudl be expose on container services for apigateway to consume. c) add Dockerfile for orrder-service d) add to docker-compose the information to build order-service

--- api-gateway/src/routes/gatewayRoutes.ts
+++ api-gateway/src/routes/gatewayRoutes.ts
@@ -1,12 +1,37 @@
-import { RequestHandler, Router } from 'express';
+import axios from 'axios';
+import { RequestHandler, Router, Request, Response } from 'express';
 import { proxyRequest } from '../controllers/gatewayController';
 import { authenticate } from '../middleware/authMIddleware';
+import {serviceMap, ServicesMapType} from '../ServiceMap';
 
 const router = Router();
 
-router.post('/createUser', proxyRequest);
+// router.post('/createUser', proxyRequest);
 
-router.get('/getAllItems', authenticate as unknown as RequestHandler, proxyRequest);
+// router.get('/getAllItems', authenticate as unknown as RequestHandler, proxyRequest);
 
+router.all('/:apiname', async (req:Request, res:Response)=>{
+    const {apiname}  = req.params;
+    console.log('Api requested: ', apiname);
+    if(apiname && apiname in serviceMap){
+        const serviceUrl:string = serviceMap[apiname as keyof ServicesMapType]
+            if(!serviceUrl){
+                return res.status(404).send('Service not Found');
+            }
 
+            try{
+                const url = `${serviceUrl}${req.originalUrl.replace(`${apiname}`,'')}`
+
+                    const response = await axios({
+                        method:req.method,
+                        url,
+                        data:req.body,
+                        headers:req.headers,
+                    });
+
+                    res.send(response.data);
+            }catch(err){console.log('ERROR: ',err)}
+    }
+    
+})
 export default router;
